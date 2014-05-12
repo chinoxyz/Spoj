9777eec347a077518abd4f5f06c22e5df84575ea
Deepak Gupta <deepakgupta1313@gmail.com>
2014-05-12 17:52:08 +0530

    Create MKEQUAL.cpp

---
+++ MKEQUAL.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <set>
+#include <queue>
+
+using namespace std;
+#define i64 long long
+
+
+int main()
+{
+    //freopen("C:/Users/Deepak/Desktop/Deepak/Programs/Spoj/Text/MKEQUAL.txt","r",stdin);
+    int cases,i,j;
+
+    i64 sum,temp,n;
+
+
+    scanf("%d",&cases);
+
+    while(cases--)
+    {
+        sum=0;
+        scanf("%lld",&n);
+
+        for(i=0;i<n;++i)
+        {
+            scanf("%lld",&temp);
+            sum+=temp;
+        }
+
+        if(sum%n)
+        {
+            printf("%lld\n",n-1);
+        }
+        else
+        {
+            printf("%lld\n",n);
+        }
+    }
+    return 0;
+}
+
+
